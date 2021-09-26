f609ec5c524e0e71a15aff94d8f6053240540d56
jonjackson <87287862+jonteal@users.noreply.github.com>
2021-09-26 01:04:09 -0500

    Set up a function to update time and an interval of 1000 ms per second

--- ASSETS/JS/script.js
+++ ASSETS/JS/script.js
@@ -1,6 +1,15 @@
 var saveButton = document.getElementById("saveBtn");
+var currentTime = moment();
+
+function updateTime() {
+    $("#currentDay").text(currentTime.format("dddd, MMMM Do"));
+}
+
+setInterval(updateClock, 1000);
 
 saveButton.addEventListener = ("click", );
+
+var currentHour = Number(currentTime.format('H'));
 
 // Function to use localStorage to set appointments that will not disappear when the page is 
 // reloaded.
@@ -10,3 +19,4 @@ saveButton.addEventListener = ("click", );
 // Function to make the current hour red, any hours that have passed the color gray,
 // and future hours green.
 
+// Function to display today's date at the top of the page
