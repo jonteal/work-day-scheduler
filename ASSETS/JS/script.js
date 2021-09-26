b1855945d8ae460006b58146bd04fbfd841015dd
jonjackson <87287862+jonteal@users.noreply.github.com>
2021-09-26 00:27:25 -0500

    Pseudo JS code started, Floppy disk symbol on all buttons

---
+++ ASSETS/JS/script.js
@@ -0,0 +1,10 @@
+var saveButton = document.getElementById("saveBtn");
+
+// Function to use localStorage to set appointments that will not disappear when the page is 
+// reloaded.
+
+// Event listener to save the appointments to the schedule
+
+// Function to make the current hour red, any hours that have passed the color gray,
+// and future hours green.
+
