a9a3befd42cdca30f086d6e41d5b1fbc6b2245d7
jonjackson <87287862+jonteal@users.noreply.github.com>
2021-09-27 07:54:17 -0500

    Added if statement for color coding hours

--- ASSETS/JS/script.js
+++ ASSETS/JS/script.js
@@ -1,15 +1,15 @@
 var saveButton = document.getElementById("saveBtn");
-var currentTime = moment();
 
-function updateTime() {
-    $("#currentDay").text(currentTime.format("dddd, MMMM Do"));
-}
+var currentDay = moment();
+$("#currentDay").text(currentDay.format("dddd, MMMM Do YYYY, h:mm:ss a"));
+
+
 
 setInterval(updateClock, 1000);
 
-saveButton.addEventListener = ("click", );
+// saveButton.addEventListener = ("click", );
 
-var currentHour = Number(currentTime.format('H'));
+var currentHour = Number(currentDay.format('H'));
 
 // Function to use localStorage to set appointments that will not disappear when the page is 
 // reloaded.
@@ -19,4 +19,9 @@ var currentHour = Number(currentTime.format('H'));
 // Function to make the current hour red, any hours that have passed the color gray,
 // and future hours green.
 
-// Function to display today's date at the top of the page
+
+if(currentHour === 9){
+    $('#0').css("background-color", "#FF0000"); 
+} else if(currentHour < 9){
+    $('#0').css("background-color", "#00FF00");
+};
