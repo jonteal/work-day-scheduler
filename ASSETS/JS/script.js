174dc2befa44fc968e023febe683b9f0ab4374d6
jonjackson <87287862+jonteal@users.noreply.github.com>
2021-09-27 12:59:46 -0500

    Added if statements for all hours on the planner

--- ASSETS/JS/script.js
+++ ASSETS/JS/script.js
@@ -25,3 +25,51 @@ if(currentHour === 9){
 } else if(currentHour < 9){
     $('#0').css("background-color", "#00FF00");
 };
+
+if(currentHour === 10){
+    $('#0').css("background-color", "#FF0000"); 
+} else if(currentHour < 10){
+    $('#0').css("background-color", "#00FF00");
+};
+
+if(currentHour === 11){
+    $('#0').css("background-color", "#FF0000"); 
+} else if(currentHour < 11){
+    $('#0').css("background-color", "#00FF00");
+};
+
+if(currentHour === 12){
+    $('#0').css("background-color", "#FF0000"); 
+} else if(currentHour < 12){
+    $('#0').css("background-color", "#00FF00");
+};
+
+if(currentHour === 1){
+    $('#0').css("background-color", "#FF0000"); 
+} else if(currentHour < 1){
+    $('#0').css("background-color", "#00FF00");
+};
+
+if(currentHour === 2){
+    $('#0').css("background-color", "#FF0000"); 
+} else if(currentHour < 2){
+    $('#0').css("background-color", "#00FF00");
+};
+
+if(currentHour === 3){
+    $('#0').css("background-color", "#FF0000"); 
+} else if(currentHour < 3){
+    $('#0').css("background-color", "#00FF00");
+};
+
+if(currentHour === 4){
+    $('#0').css("background-color", "#FF0000"); 
+} else if(currentHour < 4){
+    $('#0').css("background-color", "#00FF00");
+};
+
+if(currentHour === 5){
+    $('#0').css("background-color", "#FF0000"); 
+} else if(currentHour < 5){
+    $('#0').css("background-color", "#00FF00");
+};
