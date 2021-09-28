8556703115efe656b6e11926d617425aa2707dd8
jonjackson <87287862+jonteal@users.noreply.github.com>
2021-09-28 07:55:56 -0500

    Added ID's to all buttons

--- ASSETS/JS/script.js
+++ ASSETS/JS/script.js
@@ -3,22 +3,20 @@ var saveButton = document.getElementById("saveBtn");
 var currentDay = moment();
 $("#currentDay").text(currentDay.format("dddd, MMMM Do YYYY, h:mm:ss a"));
 
-
+var currentHour = Number(currentDay.format('H'));
 
 setInterval(updateClock, 1000);
 
+// Event listener to save the appointments to the schedule
 // saveButton.addEventListener = ("click", );
 
-var currentHour = Number(currentDay.format('H'));
 
 // Function to use localStorage to set appointments that will not disappear when the page is 
 // reloaded.
 
-// Event listener to save the appointments to the schedule
 
 // Function to make the current hour red, any hours that have passed the color gray,
 // and future hours green.
-
 
 if(currentHour === 9){
     $('#0').css("background-color", "#FF0000"); 
@@ -73,3 +71,6 @@ if(currentHour === 17){
 } else if(currentHour < 17){
     $('#8').css("background-color", "#00FF00");
 };
+
+
+$
